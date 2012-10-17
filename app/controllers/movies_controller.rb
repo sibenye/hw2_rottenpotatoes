eab90a44eabb5e3c98aa87ed4ca50a49e8804a50
saasbook <saasbook@saasbook.(none)>
2012-10-17 15:41:18 -0700

    init

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -7,11 +7,36 @@ class MoviesController < ApplicationController
   end
 
   def index
+    checkRedirect = false
+    if (session[:ratings] !=nil or session[:sort] != nil) then
+    
+    if (session[:ratings] != nil) then
+    if (params[:ratings] == nil) then
+    params[:ratings] = session[:ratings]
+    checkRedirect = true
+    end
+    end
+
+    if (session[:sort] != nil) then
+    if (params[:id] == nil) then
+    params[:id] = session[:sort]
+    checkRedirect = true
+    end
+    end
+    
+    if (checkRedirect == true) then
+    flash.keep
+    redirect_to :action => "index", :id => params[:id], :ratings => params[:ratings]
+    end
+    end
+
+
+
     @ratings = params[:ratings]
      if (@ratings != nil) then
      @checkedBox = params[:ratings]
      @checked = @checkedBox.keys
-     #@checked = ["R", "G"]
+     session[:ratings] = params[:ratings]
      @all_ratings = Movie.getRatingsList
      else
      @all_ratings = Movie.getRatingsList
@@ -22,9 +47,11 @@ class MoviesController < ApplicationController
      if (id.eql?("by-title")) then
      @movies = Movie.where(:rating => @checked).order("title ASC")
      @bytitle = "hilite"
+     session[:sort] = id
      elsif (id.eql?('by-date')) then
      @movies = Movie.where(:rating => @checked).order("release_date ASC")
      @bydate = "hilite"
+     session[:sort] = id
      else 
      @movies = Movie.where(:rating => @checked)
      end 
